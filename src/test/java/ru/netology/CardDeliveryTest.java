7a146bef897ba795b687e42b32487a687f9a139d
Elena Fedoseeva <horungeelena@gmail.com>
2021-02-19 22:30:24 +0200

    Created new test.

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,46 +1,40 @@
 package ru.netology;
 
 import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
 
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
     @Test
     void shouldDeliveryByCardForDifferentDate() {
-        open("http://localhost:9999");
-        $("[data-test-id=city] input").setValue(DataGenerator.Registration.generateByCustomer("ru").getCity());
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(DataGenerator.Registration.forwardDate(4));
-        $("[data-test-id=name] input").setValue(DataGenerator.Registration.generateByCustomer("ru").getFullName());
-        $("[data-test-id=phone] input").setValue(DataGenerator.Registration.generateByCustomer("ru").getPhoneNumber());
-        $("[data-test-id=agreement]").click();
-        $("button.button").click();
-        $("[data-test-id='success-notification'] .notification__title")
-                .shouldHave(text("Успешно!"));
-        $("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(exactText("Встреча успешно запланирована на " + DataGenerator.Registration.forwardDate(4)))
-                .shouldBe(Condition.visible,Duration.ofMillis(15000));
-        $("[data-test-id=date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id=date] input").setValue(DataGenerator.Registration.forwardDate(6));
-        $(withText("Запланировать")).click();
-        $("[data-test-id='replan-notification'] .notification__title")
-                .shouldBe(text("Необходимо подтверждение"));
-        $("[data-test-id='replan-notification'] .notification__content")
-                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
-        $("button.button").click();
-        $("[data-test-id='success-notification'] .notification__title")
-                .shouldHave(text("Успешно!"));
-        $("[data-test-id='success-notification'] .notification__content")
-                .shouldHave(text("Встреча успешно запланирована на " + DataGenerator.Registration.forwardDate(6)))
+        RegistrationByCustomerInfo user = DataGenerator.Registration.generateByCustomer("ru");
+        $("[data-test-id='city'] input").setValue(user.getCity());
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(DataGenerator.Registration.forwardDate(3));
+        $("[data-test-id='name'] input").setValue(DataGenerator.Registration.generateByCustomer("ru").getFullName());
+        $("[data-test-id='phone'] input").setValue(DataGenerator.Registration.generateByCustomer("ru").getPhoneNumber());
+        $("[data-test-id='agreement']").click();
+        $$("[type='button']").find(exactText("Запланировать")).click();
+        $(withText(DataGenerator.Registration.forwardDate(5)));
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.CONTROL, "a") + Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(DataGenerator.Registration.forwardDate(5));
+        $$("[type='button']").find(exactText("Запланировать")).click();
+        $$("[type='button']").find(exactText("Перепланировать")).click();
+        $(byText(DataGenerator.Registration.forwardDate(5))).shouldHave(text("Встреча успешно запланирована на " + DataGenerator.Registration.forwardDate(5)))
                 .shouldBe(Condition.visible,Duration.ofMillis(15000));
     }
 }
